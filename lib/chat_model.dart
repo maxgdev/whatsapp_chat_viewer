b3b3be3b3a89f5035f3abe7471aabb2eed82fb36
maxgdev <max@maxgdev.net>
2021-03-08 23:19:40 +0000

    Added ChatModel in chat_model.dart

---
+++ lib/chat_model.dart
@@ -0,0 +1,15 @@
+class ChatModel {
+  ChatModel({
+    this.date,
+    this.time,
+    this.name,
+    this.message,
+    this.fileAttached,
+  });
+
+  int date;
+  int time;
+  String name;
+  String message;
+  String fileAttached;
+}
