f6d43bb2a6995a2415f964051533e6f43e3fdfe6
maxgdev <max@maxgdev.net>
2021-03-09 14:16:10 +0000

    Changed generated chatsList with unique lorim ipsum data

--- lib/chats.dart
+++ lib/chats.dart
@@ -31,15 +31,87 @@ class ChatsState extends State<Chats> {
   // ];
 
   // Generate List of 10 Chat Objects in chatsList
-  final chatsList = List<Chat>.generate(
-      10,
-      (index) => Chat(
-          date: "1/24/21",
-          time: "11:27 AM",
-          name: "Malcolm",
-          message: "Praesent ut malesuada nulla. Nunc a sapien vitae sem",
-          fileAttached: ""));
+  // final chatsList = List<Chat>.generate(
+  //     10,
+  //     (index) => Chat(
+  //         date: "1/24/21",
+  //         time: "11:27 AM",
+  //         name: "Malcolm",
+  //         message: "Praesent ut malesuada nulla. Nunc a sapien vitae sem",
+  //         fileAttached: ""));
+  final List<Chat>chatsList = [
+    Chat(
+      date: "1/24/21",
+      time: "11:27 AM",
+      name: "Gates",
+      message: "I am a meglomaniacal, genocidal racist billiaare twat",
+      fileAttached: ""
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "11:30 AM",
+      name: "Shaka",
+      message: " Curabitur mattis vulputate feugiat",
+      fileAttached: ""
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "11:37 AM",
+      name: "Diop",
+      message: "Nam tincidunt velit sem, aliquam rhoncus massa feugiat vitae",
+      fileAttached: ""
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "12:27 PM",
+      name: "Patrice",
+      message: "Fusce condimentum placerat gravida",
+      fileAttached: ""
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "12:33 PM",
+      name: "Malcolm",
+      message: "Duis dapibus imperdiet tempus. Pellentesque rutrum a ex",
+      fileAttached: ""
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "01:07 PM",
+      name: "Garvey",
+      message: "Morbi dictum nulla id turpis lacinia eleifend",
+      fileAttached: "Budget2021.xls"
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "01:27 PM",
+      name: "Anthony",
+      message: "Aenean auctor fermentum dolor, sed hendrerit augue cursus eget",
+      fileAttached: ""
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "02:15 PM",
+      name: "Belson",
+      message: "Fusce magna massa, auctor vitae ipsum in",
+      fileAttached: ""
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "03:01 PM",
+      name: "Boris",
+      message: "I am the biggest lying bullshiting Prime Minister ever",
+      fileAttached: ""
+    ),
+    Chat(
+      date: "1/24/21",
+      time: "02:27 PM",
+      name: "Hancock",
+      message: "Call me a COVID twat and dump me in the deepest hell prison",
+      fileAttached: ""
+    ),                                
 
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
