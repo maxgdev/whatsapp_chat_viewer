6ae010bf80a3b040ef44b076070fb515eddbf5e4
maxgdev <max@maxgdev.net>
2021-03-15 20:23:53 +0000

    ChatDetailsScreen now renders all chat bubbles dynamically in ListView builder

--- lib/chat_details.dart
+++ lib/chat_details.dart
@@ -87,104 +87,53 @@ class _ChatDetailsScreenState extends State<ChatDetailsScreen> {
           ),
         ),
         // color: Colors.yellow.withAlpha(64),
-        child: ListView(
-          padding: EdgeInsets.all(8),
-          children: [
-            Bubble(
-              alignment: Alignment.center,
-              color: Color.fromARGB(255, 212, 234, 244),
-              borderColor: Colors.black,
-              borderWidth: 2,
-              margin: BubbleEdges.only(top: 8),
-              child: Text(
-                'TODAY',
-                style: TextStyle(fontSize: 10),
-              ),
-            ),
-            Bubble(
-              style: styleSomebody,
-              child: Text(
-                  'Hi Jason. Sorry to bother you. I have a queston for you.'),
-            ),
-            Bubble(
-              style: styleMe,
-              child: Text("Whats'up?"),
-            ),
-            Bubble(
-              style: styleSomebody,
-              child: Text("I've been having a problem with my computer."),
-            ),
-            Bubble(
-              style: styleSomebody,
-              margin: BubbleEdges.only(top: 4),
-              showNip: false,
-              child: Text('Can you help me?'),
-            ),
-            Bubble(
-              style: styleMe,
-              child: Text('Ok'),
-            ),
-            Bubble(
-              style: styleMe,
-              showNip: false,
-              margin: BubbleEdges.only(top: 4),
-              child: Text("What's the problem?"),
-            ),
-  
-            Divider(),
-    
-            Container(
-              height: 600,
-              child: ListView.builder(
-                itemCount: _chatConversation.length,
-                itemBuilder: (context, index) {
-                  // return Text(_chatConversation[index]);
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        // CircleAvatar(
-                        //   backgroundColor: ChatColors.whatsAppGreen,
-                        //   child: Text("${parseLine(_chatConversation[index], 2)[1]}"),
-                        // ),
-                        Expanded(
-                          child: Bubble(
-                            style: 
-                              parseLine(_chatConversation[index], 2).trim() ==  _selfName 
-                                ? styleMe 
-                                : styleSomebody, 
-                            margin: BubbleEdges.only(top: 4),
-                            showNip: true,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("${parseLine(_chatConversation[index], 2)}",
-                                style: TextStyle(fontWeight: FontWeight.w800, color: ChatColors.whatsAppGreen),
-                                ),
-                                Text("${parseLine(_chatConversation[index], 3)}"
-                                  ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Expanded(child: Text("${parseLine(_chatConversation[index], 0)}",
-                                    style: TextStyle(fontSize: 9))),
-                                    Text("${parseLine(_chatConversation[index], 1)}",
-                                    style: TextStyle(fontSize: 9)
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
+        child: ListView.builder(
+          itemCount: _chatConversation.length,
+          itemBuilder: (context, index) {
+            // return Text(_chatConversation[index]);
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  // CircleAvatar(
+                  //   backgroundColor: ChatColors.whatsAppGreen,
+                  //   child: Text("${parseLine(_chatConversation[index], 2)[1]}"),
+                  // ),
+                  Expanded(
+                    child: Bubble(
+                      style: 
+                        parseLine(_chatConversation[index], 2).trim() ==  _selfName 
+                          ? styleMe 
+                          : styleSomebody, 
+                      margin: BubbleEdges.only(top: 4),
+                      showNip: true,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("${parseLine(_chatConversation[index], 2)}",
+                          style: TextStyle(fontWeight: FontWeight.w800, color: ChatColors.whatsAppGreen),
                           ),
-                        ),
-                      ],
+                          Text("${parseLine(_chatConversation[index], 3)}"
+                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Expanded(child: Text("${parseLine(_chatConversation[index], 0)}",
+                              style: TextStyle(fontSize: 9))),
+                              Text("${parseLine(_chatConversation[index], 1)}",
+                              style: TextStyle(fontSize: 9)
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  );
-                },
+                  ),
+                ],
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
